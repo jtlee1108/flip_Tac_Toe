3ddb554356f446a8935980cb86554f004e6cddc3
Chris Thrasher <chris.j.thrasher@gmail.com>
2018-05-13 11:42:35 -0600

    Refactored series of if statements to use switch statement

--- flipTacToe/ViewController.swift
+++ flipTacToe/ViewController.swift
@@ -8,19 +8,18 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    
+class ViewController: UIViewController
+{
     var playerTurn: Bool = true
     let GravDirection = ["Down","Right","Up","Left"]
     var Direction = 0
     var BoardArray = Array(repeating: Array(repeating: 0, count: 7), count: 7)
     var PointArray = Array(repeating: Array(repeating: 0, count: 7), count: 7)
-    var XCount = 0
-    var YCount = 0
-    var counter = 1
+    var XCount: Int = 0
+    var YCount: Int = 0
+    var counter: Int = 1
     var sign = 1 //Determines the display according to which player makes the move
-    var triggerCount = 0
+    var triggerCount: Int = 0
     let colorRed = UIColor(red: 1, green:0, blue: 0, alpha: 1)
     let colorBlack = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
     var location = 0
@@ -34,18 +33,22 @@ class ViewController: UIViewController {
     @IBOutlet weak var PlayerTurnLabel: UILabel!
     
     
-    @IBAction func RotateCCW(_ sender: UIButton) {
+    @IBAction func RotateCCW(_ sender: UIButton)
+    {
         if (Direction < 3) { Direction = Direction+1}
         else {Direction = 0}
         RunGame()
     }
-    @IBAction func RotateCW(_ sender: UIButton) {
+    
+    @IBAction func RotateCW(_ sender: UIButton)
+    {
         if (Direction > 0) { Direction = Direction-1}
         else {Direction = 3}
         RunGame()
     }
     
-    @IBAction func ResetButton(_ sender: UIButton) {
+    @IBAction func ResetButton(_ sender: UIButton)
+    {
         counter = 1
         XCount = 0
         YCount = 0
@@ -56,7 +59,8 @@ class ViewController: UIViewController {
             BoardArray[XCount][YCount] = 0
             YCount = YCount + 1
             
-            if YCount > 6{
+            if YCount > 6
+            {
                 YCount = 0
                 XCount = XCount+1
             }
@@ -67,49 +71,57 @@ class ViewController: UIViewController {
         playerTurn = true
         PlayerTurnLabel.text = "Player 1"
     }
-    
-    
+
     @IBAction func TopButton(_ sender: UIButton) {
-        if Direction == 0 && BoardArray[sender.tag - 1] [6]==0{
+        if Direction == 0 && BoardArray[sender.tag - 1] [6] == 0
+        {
             BoardArray[sender.tag - 1] [6] = sign
             RunGame()
-        }}
+        }
+    }
     
     @IBAction func LeftButton(_ sender: UIButton) {
-        if Direction == 1 && BoardArray[0] [sender.tag - 1]==0{
+        if Direction == 1 && BoardArray[0] [sender.tag - 1] == 0
+        {
             BoardArray[0] [sender.tag - 1] = sign
             RunGame()
-        }}
+        }
+    }
     
     @IBAction func BottomButton(_ sender: UIButton) {
-        if Direction == 2 && BoardArray[sender.tag - 1] [0]==0{
+        if Direction == 2 && BoardArray[sender.tag - 1] [0] == 0
+        {
             BoardArray[sender.tag - 1] [0] = sign
             RunGame()
         }
     }
     
     @IBAction func RightButton(_ sender: UIButton) {
-        if Direction == 3 && BoardArray[6] [sender.tag - 1]==0{
+        if Direction == 3 && BoardArray[6] [sender.tag - 1] == 0
+        {
             BoardArray[6] [sender.tag - 1] = sign
             RunGame()
-        }}
+        }
+    }
     
-    func RunGame(){
-        
+    func RunGame()
+    {
         RunGravity()
         ChangeSquares()
         CheckForPoints()
         playerTurn = !playerTurn
-        if playerTurn {PlayerTurnLabel.text = "Player 1"
+        if playerTurn
+        {
+            PlayerTurnLabel.text = "Player 1"
             sign = 1
         }
-        else {PlayerTurnLabel.text = "Player 2"
+        else
+        {
+            PlayerTurnLabel.text = "Player 2"
             sign = -1
         }
         GravityDirectionLabel.text = GravDirection[Direction]
-        
-        
-    }// End Run Game ============================================
+    } // End RunGame()
     
     func ChangeSquares()
     {
@@ -117,8 +129,8 @@ class ViewController: UIViewController {
         YCount = 0
         counter  = 1
         
-        while counter <= 49{
-            
+        while counter <= 49
+        {
             switch BoardArray[XCount][YCount]{
             case 1:
                 Squares[counter - 1].text = "O"
@@ -130,7 +142,8 @@ class ViewController: UIViewController {
             
             counter = counter + 1
             XCount = XCount + 1
-            if XCount > 6{
+            if XCount > 6
+            {
                 XCount = 0
                 YCount = YCount + 1
             }
@@ -141,14 +154,17 @@ class ViewController: UIViewController {
     {
         counter = 1
         triggerCount = 0
-        // Gravity Down
-        if Direction == 0{
+        switch Direction
+        {
+        case 0: // Gravity Down
             XCount = 0
             YCount = 0
             while counter <= 49
             {
-                if BoardArray[XCount][YCount] != 0{
-                    if triggerCount != YCount{
+                if BoardArray[XCount][YCount] != 0
+                {
+                    if triggerCount != YCount
+                    {
                         BoardArray[XCount][triggerCount] = BoardArray[XCount][YCount]
                         BoardArray[XCount][YCount] = 0
                     }
@@ -156,15 +172,15 @@ class ViewController: UIViewController {
                 }
                 YCount = YCount + 1
                 
-                if YCount > 6{
+                if YCount > 6
+                {
                     YCount = 0
                     XCount = XCount+1
                     triggerCount = 0
                 }
                 counter = counter+1
-            }}
-        //Gravity Right
-        if Direction == 1{
+            }
+        case 1: //Gravity Right
             XCount = 6
             YCount = 0
             while counter <= 49
@@ -184,15 +200,16 @@ class ViewController: UIViewController {
                     triggerCount = 0
                 }
                 counter = counter+1
-            }}
-        //Gravity Up
-        if Direction == 2{
+            }
+        case 2: //Gravity Up
             XCount = 0
             YCount = 6
             while counter <= 49
             {
-                if BoardArray[XCount][YCount] != 0{
-                    if triggerCount != (6-YCount){
+                if BoardArray[XCount][YCount] != 0
+                {
+                    if triggerCount != (6-YCount)
+                    {
                         BoardArray[XCount][6-triggerCount] = BoardArray[XCount][YCount]
                         BoardArray[XCount][YCount] = 0
                     }
@@ -200,52 +217,62 @@ class ViewController: UIViewController {
                 }
                 YCount = YCount - 1
                 
-                if YCount < 0{
+                if YCount < 0
+                {
                     YCount = 6
-                    XCount = XCount+1
+                    XCount += 1
                     triggerCount = 0
                 }
-                counter = counter+1
-            }}
-        //Gravity Left
-        if Direction == 3{
+                counter += 1
+            }
+        case 3: //Gravity Left
             XCount = 0
             YCount = 0
             while counter <= 49
             {
-                if BoardArray[XCount][YCount] != 0{
-                    if triggerCount != (XCount){
+                if BoardArray[XCount][YCount] != 0
+                {
+                    if triggerCount != (XCount)
+                    {
                         BoardArray[triggerCount][YCount] = BoardArray[XCount][YCount]
                         BoardArray[XCount][YCount] = 0
                     }
-                    triggerCount = triggerCount + 1
+                    triggerCount += 1
                 }
                 XCount = XCount + 1
                 
-                if XCount > 6{
+                if XCount > 6
+                {
                     XCount = 0
-                    YCount = YCount+1
+                    YCount += 1
                     triggerCount = 0
                 }
                 counter = counter+1
-            }}
+            }
+        default: // Should eventually raise error
+            print("Direction \(Direction) not valid")
+        }
     }
     
-    func CheckForPoints(){
+    func CheckForPoints()
+    {
         //First i am setting point array back to zeros
-        for XCount in [0 , 1 , 2 , 3 , 4 , 5 , 6]{
-            for YCount in [0 , 1 , 2 , 3 , 4 , 5 , 6]{
+        for XCount in [0 , 1 , 2 , 3 , 4 , 5 , 6]
+        {
+            for YCount in [0 , 1 , 2 , 3 , 4 , 5 , 6]
+            {
                 PointArray[XCount][YCount] = 0
-            }}
+            }
+        }
         
-        for checkCount in [-1 , 1]{
+        for checkCount in [-1 , 1] {
             counter = 1
             XCount = 0
             YCount = 6
-            while counter <= 49{
-                if BoardArray[XCount][YCount] == checkCount{
+            while counter <= 49 {
+                if BoardArray[XCount][YCount] == checkCount {
                     //Check for vertical winner
-                    if YCount >= 3{
+                    if YCount >= 3 {
                         if abs(BoardArray[XCount][YCount] + BoardArray[XCount][YCount - 1] + BoardArray[XCount][YCount - 2] + BoardArray[XCount][YCount - 3]) == 4 {
                             qCounter = 1
                             PointArray[XCount][YCount]      = 1
