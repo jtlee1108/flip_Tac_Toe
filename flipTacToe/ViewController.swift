a622c79bc03912ef516292241cfa6864a2bdc060
Chris Thrasher <chris.j.thrasher@gmail.com>
2018-05-14 20:08:36 -0600

    Refactored variable names and loops

--- flipTacToe/ViewController.swift
+++ flipTacToe/ViewController.swift
@@ -11,109 +11,108 @@ import UIKit
 class ViewController: UIViewController
 {
     var playerTurn: Bool = true
-    let GravDirection = ["Down","Right","Up","Left"]
-    var Direction: Int = 0
-    var BoardArray = Array(repeating: Array(repeating: 0, count: 7), count: 7)
-    var PointArray = Array(repeating: Array(repeating: 0, count: 7), count: 7)
+    let gravDirection = ["Down","Right","Up","Left"]
+    var direction: Int = 0
+    var boardArray = Array(repeating: Array(repeating: 0, count: 7), count: 7)
+    var pointArray = Array(repeating: Array(repeating: 0, count: 7), count: 7)
     var sign: Int = 1 //Determines the display according to which player makes the move
     let colorRed = UIColor(red: 1, green:0, blue: 0, alpha: 1)
     let colorBlack = UIColor(red: 0, green: 0, blue: 0, alpha: 1)
     let colorBlue = UIColor(red: 0, green: 0, blue: 1, alpha: 1)
     var checkCount: Int = 0
-    var Player1Score: Int = 0
-    var Player2Score: Int = 0
+    var player1Score: Int = 0
+    var player2Score: Int = 0
     var pointsAdded = false
     
     @IBOutlet weak var DebugLabel: UILabel!
-    
-    
     @IBOutlet var ScoreBoard: [UILabel]!
     @IBOutlet var Squares: [UILabel]!
     @IBOutlet weak var GravityDirectionLabel: UILabel!
     @IBOutlet weak var PlayerTurnLabel: UILabel!
     
-    
     @IBAction func RotateCCW(_ sender: UIButton)
     {
-        if (Direction < 3) { Direction += 1}
-        else {Direction = 0}
-        RunGame()
+        if (direction < 3) { direction += 1}
+        else {direction = 0}
+        runGame()
     }
     
     @IBAction func RotateCW(_ sender: UIButton)
     {
-        if (Direction > 0) { Direction -= 1}
-        else {Direction = 3}
-        RunGame()
+        if (direction > 0) { direction -= 1}
+        else {direction = 3}
+        runGame()
     }
     
     @IBAction func ResetButton(_ sender: UIButton)
     {
-        for ResetXCount in 0...6
+        for i in 0...6
         {
-            for ResetYCount in 0...6
+            for j in 0...6
             {
-                BoardArray[ResetXCount][ResetYCount] = 0
+                boardArray[i][j] = 0
             }
         }
-        Direction = 0
-        RunGame()
+        direction = 0
+        runGame()
         playerTurn = true
         PlayerTurnLabel.text = "Player 1"
         PlayerTurnLabel.textColor = colorRed
     }
     
-    @IBAction func TopButton(_ sender: UIButton) {
-        if Direction == 0 && BoardArray[sender.tag - 1] [6] == 0
+    @IBAction func TopButton(_ sender: UIButton)
+    {
+        if direction == 0 && boardArray[sender.tag - 1] [6] == 0
         {
-            BoardArray[sender.tag - 1] [6] = sign
-            RunGame()
+            boardArray[sender.tag - 1] [6] = sign
+            runGame()
         }
     }
     @IBOutlet var TopButtonOutlet: [UIButton]!
     
-    
-    @IBAction func LeftButton(_ sender: UIButton) {
-        if Direction == 1 && BoardArray[0] [sender.tag - 1] == 0
+    @IBAction func LeftButton(_ sender: UIButton)
+    {
+        if direction == 1 && boardArray[0] [sender.tag - 1] == 0
         {
-            BoardArray[0] [sender.tag - 1] = sign
-            RunGame()
+            boardArray[0] [sender.tag - 1] = sign
+            runGame()
         }
     }
     @IBOutlet var LeftButtonOutlet: [UIButton]!
     
-    
-    @IBAction func BottomButton(_ sender: UIButton) {
-        if Direction == 2 && BoardArray[sender.tag - 1] [0] == 0
+    @IBAction func BottomButton(_ sender: UIButton)
+    {
+        if direction == 2 && boardArray[sender.tag - 1] [0] == 0
         {
-            BoardArray[sender.tag - 1] [0] = sign
-            RunGame()
+            boardArray[sender.tag - 1] [0] = sign
+            runGame()
         }
     }
     @IBOutlet var BottomButtonOutlet: [UIButton]!
     
-    @IBAction func RightButton(_ sender: UIButton) {
-        if Direction == 3 && BoardArray[6] [sender.tag - 1] == 0
+    @IBAction func RightButton(_ sender: UIButton)
+    {
+        if direction == 3 && boardArray[6] [sender.tag - 1] == 0
         {
-            BoardArray[6] [sender.tag - 1] = sign
-            RunGame()
+            boardArray[6] [sender.tag - 1] = sign
+            runGame()
         }
     }
     @IBOutlet var RightButtonOutlet: [UIButton]!
     
-    
-    func RunGame()
+    func runGame()
     {
-        RunGravity()
-        ChangeSquares()
+        runGravity()
+        changeSquares()
         
-        CheckForPoints()
+        checkForPoints()
         
-        while pointsAdded{
-            RunGravity()
+        while pointsAdded
+        {
+            runGravity()
             pointsAdded = false
-            ChangeSquares()
-            CheckForPoints()
+            changeSquares()
+            checkForPoints()
         }
         playerTurn = !playerTurn
         if playerTurn
@@ -128,10 +127,10 @@ class ViewController: UIViewController
             sign = -1
             PlayerTurnLabel.textColor = colorBlue
         }
-        GravityDirectionLabel.text = GravDirection[Direction]
+        GravityDirectionLabel.text = gravDirection[direction]
         for i in 0...6
         {
-            switch Direction
+            switch direction
             {
             case 0:
                 TopButtonOutlet[i].isHidden = false
@@ -155,194 +154,195 @@ class ViewController: UIViewController
                 RightButtonOutlet[i].isHidden = false
             }
         }
-        ScoreBoard[0].text = "Player1 : " + String(Player1Score)
+        ScoreBoard[0].text = "Player1 : " + String(player1Score)
         ScoreBoard[0].textColor = colorRed
-        ScoreBoard[1].text = "Player2 : " + String(Player2Score)
+        ScoreBoard[1].text = "Player2 : " + String(player2Score)
         ScoreBoard[1].textColor = colorBlue
-    } // End RunGame()
+    } // End runGame()
     
-    func ChangeSquares()
+    func changeSquares()
     {
-        var ChangeLocation = 0
-        for ChangeXCount in 0...6
+        var changeLocation = 0
+        for i in 0...6
         {
-            for ChangeYCount in 0...6
+            for j in 0...6
             {
-                ChangeLocation = (7 * ChangeYCount) + ChangeXCount
-                switch BoardArray[ChangeXCount][ChangeYCount]{
+                changeLocation = (7 * j) + i
+                switch boardArray[i][j]{
                 case 1:
-                    Squares[ChangeLocation].text = "O"
-                    Squares[ChangeLocation].textColor = colorRed
+                    Squares[changeLocation].text = "O"
+                    Squares[changeLocation].textColor = colorRed
                 case -1:
-                    Squares[ChangeLocation].text = "X"
-                    Squares[ChangeLocation].textColor = colorBlue
+                    Squares[changeLocation].text = "X"
+                    Squares[changeLocation].textColor = colorBlue
                 default:
-                    Squares[ChangeLocation].text = "  "
+                    Squares[changeLocation].text = "  "
                 }
             }
         }
     }
-    func RunGravity()
+    
+    func runGravity()
     {
-        var GravTriggerCount = 0
-        switch Direction
+        var gravTriggerCount = 0
+        switch direction
         {
         case 0: // Gravity Down
-            for GravXCount in 0...6
+            for i in 0...6
             {
-                for GravYCount in 0...6
+                for j in 0...6
                 {
-                    if BoardArray[GravXCount][GravYCount] != 0
+                    if boardArray[i][j] != 0
                     {
-                        if GravTriggerCount != GravYCount
+                        if gravTriggerCount != j
                         {
-                            BoardArray[GravXCount][GravTriggerCount] = BoardArray[GravXCount][GravYCount]
-                            BoardArray[GravXCount][GravYCount] = 0
+                            boardArray[i][gravTriggerCount] = boardArray[i][j]
+                            boardArray[i][j] = 0
                         }
-                        GravTriggerCount += 1
+                        gravTriggerCount += 1
                     }
                 }
-                GravTriggerCount = 0
+                gravTriggerCount = 0
             }
         case 1: //Gravity Right
-            for GravYCount in 0...6
+            for j in 0...6
             {
-                for GravXCount in [6,5,4,3,2,1,0]
+                for i in (0...6).reversed()
                 {
-                    if BoardArray[GravXCount][GravYCount] != 0{
-                        if GravTriggerCount != (6-GravXCount){
-                            BoardArray[6-GravTriggerCount][GravYCount] = BoardArray[GravXCount][GravYCount]
-                            BoardArray[GravXCount][GravYCount] = 0
+                    if boardArray[i][j] != 0{
+                        if gravTriggerCount != (6-i){
+                            boardArray[6-gravTriggerCount][j] = boardArray[i][j]
+                            boardArray[i][j] = 0
                         }
-                        GravTriggerCount += 1
+                        gravTriggerCount += 1
                     }
                 }
-                GravTriggerCount = 0
+                gravTriggerCount = 0
             }
         case 2: //Gravity Up
-            for GravXCount in 0...6
+            for i in 0...6
             {
-                for GravYCount in [6,5,4,3,2,1,0]
+                for j in (0...6).reversed()
                 {
-                    if BoardArray[GravXCount][GravYCount] != 0
+                    if boardArray[i][j] != 0
                     {
-                        if GravTriggerCount != (6-GravYCount)
+                        if gravTriggerCount != (6-j)
                         {
-                            BoardArray[GravXCount][6-GravTriggerCount] = BoardArray[GravXCount][GravYCount]
-                            BoardArray[GravXCount][GravYCount] = 0
+                            boardArray[i][6-gravTriggerCount] = boardArray[i][j]
+                            boardArray[i][j] = 0
                         }
-                        GravTriggerCount += 1
+                        gravTriggerCount += 1
                     }
                 }
-                GravTriggerCount = 0
+                gravTriggerCount = 0
             }
         case 3: //Gravity Left
-            for GravYCount in 0...6
+            for j in 0...6
             {
-                for GravXCount in 0...6
+                for i in 0...6
                 {
-                    if BoardArray[GravXCount][GravYCount] != 0
+                    if boardArray[i][j] != 0
                     {
-                        if GravTriggerCount != (GravXCount)
+                        if gravTriggerCount != (i)
                         {
-                            BoardArray[GravTriggerCount][GravYCount] = BoardArray[GravXCount][GravYCount]
-                            BoardArray[GravXCount][GravYCount] = 0
+                            boardArray[gravTriggerCount][j] = boardArray[i][j]
+                            boardArray[i][j] = 0
                         }
-                        GravTriggerCount += 1
+                        gravTriggerCount += 1
                     }
                 }
-                GravTriggerCount = 0
+                gravTriggerCount = 0
             }
         default: // Should eventually raise error
-            print("Direction \(Direction) not valid")
+            print("Direction \(direction) not valid")
         }
     }
     
-    func CheckForPoints()
+    func checkForPoints()
     {
         //First i am setting point array back to zeros
-        for CheckXCount in [0,1,2,3,4,5,6]
+        for i in 0...6
         {
-            for CheckYCount in 0...6
+            for j in 0...6
             {
-                PointArray[CheckXCount][CheckYCount] = 0
+                pointArray[i][j] = 0
             }
         }
         
         for PlayerCount in [-1 , 1]
         {
-            for CheckXCount in 0...6
+            for i in 0...6
             {
-                for CheckYCount in [6,5,4,3,2,1,0]
+                for j in (0...6).reversed()
                 {
-                    if BoardArray[CheckXCount][CheckYCount] == PlayerCount
+                    if boardArray[i][j] == PlayerCount
                     {
                         //Check for Vertical
-                        if CheckYCount >= 3
+                        if j >= 3
                         {
-                            if abs(BoardArray[CheckXCount][CheckYCount] + BoardArray[CheckXCount][CheckYCount - 1] + BoardArray[CheckXCount][CheckYCount - 2] + BoardArray[CheckXCount][CheckYCount - 3]) == 4
+                            if abs(boardArray[i][j] + boardArray[i][j - 1] + boardArray[i][j - 2] + boardArray[i][j - 3]) == 4
                             {
                                 switch PlayerCount{
                                 case -1:
-                                    Player2Score += 1
+                                    player2Score += 1
                                 default:
-                                    Player1Score += 1
+                                    player1Score += 1
                                 }
-                                PointArray[CheckXCount][CheckYCount]      = 1
-                                PointArray[CheckXCount][CheckYCount - 1]  = 1
-                                PointArray[CheckXCount][CheckYCount - 2]  = 1
-                                PointArray[CheckXCount][CheckYCount - 3]  = 1
+                                pointArray[i][j]      = 1
+                                pointArray[i][j - 1]  = 1
+                                pointArray[i][j - 2]  = 1
+                                pointArray[i][j - 3]  = 1
                             }
                         }
                         //Check for Horizontal
-                        if CheckXCount <= 3
+                        if i <= 3
                         {
-                            if abs(BoardArray[CheckXCount][CheckYCount] + BoardArray[CheckXCount + 1][CheckYCount] + BoardArray[CheckXCount + 2][CheckYCount] + BoardArray[CheckXCount + 3][CheckYCount]) == 4
+                            if abs(boardArray[i][j] + boardArray[i + 1][j] + boardArray[i + 2][j] + boardArray[i + 3][j]) == 4
                             {
                                 switch PlayerCount{
                                 case -1:
-                                    Player2Score += 1
+                                    player2Score += 1
                                 default:
-                                    Player1Score += 1
+                                    player1Score += 1
                                 }
-                                PointArray[CheckXCount][CheckYCount]      = 1
-                                PointArray[CheckXCount + 1][CheckYCount]  = 1
-                                PointArray[CheckXCount + 2][CheckYCount]  = 1
-                                PointArray[CheckXCount + 3][CheckYCount]  = 1
+                                pointArray[i][j]      = 1
+                                pointArray[i + 1][j]  = 1
+                                pointArray[i + 2][j]  = 1
+                                pointArray[i + 3][j]  = 1
                             }
                         }
                         //Check for cross \
-                        if CheckXCount <= 3 && CheckYCount >= 3
+                        if i <= 3 && j >= 3
                         {
-                            if abs(BoardArray[CheckXCount][CheckYCount] + BoardArray[CheckXCount + 1][CheckYCount - 1] + BoardArray[CheckXCount + 2][CheckYCount - 2] + BoardArray[CheckXCount + 3][CheckYCount - 3]) == 4
+                            if abs(boardArray[i][j] + boardArray[i + 1][j - 1] + boardArray[i + 2][j - 2] + boardArray[i + 3][j - 3]) == 4
                             {
                                 switch PlayerCount{
                                 case -1:
-                                    Player2Score += 1
+                                    player2Score += 1
                                 default:
-                                    Player1Score += 1
+                                    player1Score += 1
                                 }
-                                PointArray[CheckXCount][CheckYCount]      = 1
-                                PointArray[CheckXCount + 1][CheckYCount - 1]  = 1
-                                PointArray[CheckXCount + 2][CheckYCount - 2]  = 1
-                                PointArray[CheckXCount + 3][CheckYCount - 3]  = 1
+                                pointArray[i][j]          = 1
+                                pointArray[i + 1][j - 1]  = 1
+                                pointArray[i + 2][j - 2]  = 1
+                                pointArray[i + 3][j - 3]  = 1
                             }
                         }
                         //Check for cross /
-                        if CheckXCount >= 3 && CheckYCount >= 3
+                        if i >= 3 && j >= 3
                         {
-                            if abs(BoardArray[CheckXCount][CheckYCount] + BoardArray[CheckXCount - 1][CheckYCount - 1] + BoardArray[CheckXCount - 2][CheckYCount - 2] + BoardArray[CheckXCount - 3][CheckYCount - 3]) == 4
+                            if abs(boardArray[i][j] + boardArray[i - 1][j - 1] + boardArray[i - 2][j - 2] + boardArray[i - 3][j - 3]) == 4
                             {
                                 switch PlayerCount{
                                 case -1:
-                                    Player2Score += 1
+                                    player2Score += 1
                                 default:
-                                    Player1Score += 1
+                                    player1Score += 1
                                 }
-                                PointArray[CheckXCount][CheckYCount]      = 1
-                                PointArray[CheckXCount - 1][CheckYCount - 1]  = 1
-                                PointArray[CheckXCount - 2][CheckYCount - 2]  = 1
-                                PointArray[CheckXCount - 3][CheckYCount - 3]  = 1
+                                pointArray[i][j]          = 1
+                                pointArray[i - 1][j - 1]  = 1
+                                pointArray[i - 2][j - 2]  = 1
+                                pointArray[i - 3][j - 3]  = 1
                                 
                             }
                         }
@@ -351,19 +351,19 @@ class ViewController: UIViewController
                 } //end of CheckY loop
             } //end of for CheckX loop
         }
-        for CheckXCount in 0...6
+        for i in 0...6
         {
-            for CheckYCount in [6,5,4,3,2,1,0]
+            for j in (0...6).reversed()
             {
-                if PointArray[CheckXCount][CheckYCount] == 1 {pointsAdded = true}
+                if pointArray[i][j] == 1 {pointsAdded = true}
             }
         }
         if pointsAdded{
-            for CheckXCount in 0...6
+            for i in 0...6
             {
-                for CheckYCount in [6,5,4,3,2,1,0]
+                for j in (0...6).reversed()
                 {
-                    if PointArray[CheckXCount][CheckYCount] == 1 {BoardArray[CheckXCount][CheckYCount] = 0}
+                    if pointArray[i][j] == 1 {boardArray[i][j] = 0}
                 }
             }
         }
